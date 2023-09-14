64ff3c84bf54eee1c8fd5ce3af285e0f08ec7684
dpsiu <denversiu24@gmail.com>
2023-09-13 20:59:12 -0500

    Base Configuration

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-blue-600">Hello world!</h1>
+    </>
+  );
+}
