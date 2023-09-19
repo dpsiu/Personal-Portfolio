bdb29fe2248f6ec568a0c3c40966833e7779e7d5
dpsiu <denversiu24@gmail.com>
2023-09-19 17:01:00 -0500

    Add page and content - about page
    consists of intrduction and layout

--- Pages/About.jsx
+++ Pages/About.jsx
@@ -1,5 +1,41 @@
-
+import React from "react";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 
 export function About() {
-    return <h1>About</h1>
+  return (
+    <>
+      <section className="max-w-4xl lg:max-w-6xl lg:text-xl lg:mx-auto">
+        <p className="text-3xl">
+          Welcome! I'm <span className="text-indigo-400 font-bold">Denver</span>
+        </p>
+        <div className="py-14">
+          <p className="py-2">
+            I graduated from the University of Texas at Dallas, wielding a
+            degree in Design and Production with an emphasis on UX design.
+          </p>
+          <p className="py-2">
+            Long before university and early professional experiences, I had a
+            passion for creating and building. This led to me pursuing design
+            with an urge to create usable and enjoyable experiences!
+          </p>
+          <p className="py-2">
+            After some time working in the field, my passion for building
+            extended further. Aside from the joy of researching, empathizing,
+            and iterating on designs, I wanted the capability to actually build
+            them as well.
+          </p>
+          <p className="py-2">
+            Thus began my journey from design into software development.
+            Currently, I am in the midst of{" "}
+            <span className="hover:underline text-amber-600 dark:text-amber-400">
+              <a href="https://www.theodinproject.com/">
+                The Odin Project's Full Stack self-taught JavaScript course
+              </a>
+            </span>
+            .
+          </p>
+        </div>
+      </section>
+    </>
+  );
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import "./styles.css";
+import { useState } from "react";
 
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
@@ -7,24 +7,26 @@ import { Route, Routes, Link } from "react-router-dom";
 import { About } from "../Pages/About";
 import { Home } from "../Pages/Home";
 
-// Terminal open command = Ctrl + Shift + `
-
 export default function App() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
 
   return (
     <>
       <div className={darkMode ? "dark" : ""}>
-        <section className=" bg-white min-h-screen px-10 items-center md:px-25 lg:px-40 min-w-fit dark:bg-zinc-700 dark:text-white">
+        <section className=" text-stone-700 bg-white min-h-screen px-10 items-center md:px-25 lg:px-40 min-w-fit dark:bg-zinc-700 dark:text-white">
           <nav className="py-10 mb-12 flex justify-between items-center text-xl ">
             <ul className="flex items-center gap-4">
-              <h1 className="">Denver Siu</h1>
+              <h1>
+                <Link to="/">Denver Siu</Link>
+              </h1>
               <li className="cursor-pointer text-indigo-500 dark:text-orange-500">
-                  <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} />
-                </li>
+                <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} />
+              </li>
             </ul>
             <ul className="flex items-center">
-            <h1 className="hover:"><Link to="/Pages/About.jsx">About</Link></h1>
+              <h1 className="hover:">
+                <Link to="/about">About</Link>
+              </h1>
               <li className=" bg-gradient-to-r from-indigo-500 to-orange-500 text-white px-4 py-2 rounded-md ml-8">
                 <a href="https://drive.google.com/file/d/1DPnF6CqpTiy3mDw5hLNS6x6YxuaDcr44/view?usp=drive_link">
                   Resume
@@ -33,16 +35,11 @@ export default function App() {
             </ul>
           </nav>
           <Routes>
-            <Route path="/" element={<Home />}>
-              Home
-            </Route>
-            <Route path="Pages/About.jsx" element={<About />}>
-              About
-            </Route>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
           </Routes>
         </section>
       </div>
-      
     </>
   );
 }
