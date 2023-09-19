c4094826eb329ace59fd6128c78cc4db497b716e
dpsiu <denversiu24@gmail.com>
2023-09-19 18:47:32 -0500

    Test the image path with direct path

--- Pages/Home.jsx
+++ Pages/Home.jsx
@@ -40,7 +40,7 @@ export function Home() {
           </h3>
           <a href="https://denver-siu-shopping-cart.netlify.app/">
             <img
-              src="/shopcarthomepage.jpg"
+              src="shopcarthomepage.jpg"
               alt="Font page screenshot of the shopping cart application"
               className="rounded-lg py-4"
             />
@@ -71,7 +71,7 @@ export function Home() {
           </h3>
           <a href="https://denver-siu-cv-application.netlify.app/">
             <img
-              src="/cvapphomepage.jpg"
+              src="cvapphomepage.jpg"
               alt="Screenshot of CV Application Project's front page"
               className="rounded-lg py-4"
             />
