b97382d40ee2e1f3baa8f00067380062b1280929
dpsiu <denversiu24@gmail.com>
2023-09-26 10:54:58 -0500

    Fix issue & add skillscomponent
    Issue fixed - component styling issues. Added component folder to
        tailwind config content array

--- Components/Skills.jsx
+++ Components/Skills.jsx
@@ -1,45 +1,51 @@
 export function Skills() {
   return (
     <section>
-      <div className="text-center bg-red-500">
-        <h3 className="text-4xl pt-10 font-semibold text-indigo-500 dark:text-indigo-400 pb-6">
+      <div className="text-center">
+        <h3 className="text-4xl py-10 font-semibold text-indigo-500 dark:text-indigo-400 pb-6">
           Skills
         </h3>
-        <section className="text-4xl flex justify-center items-center gap-4 flex-wrap">
-          <div className="flex-col justify-between items-center gap-4 h-16">
-            <span className="devicon-github-original dark:text-stone-700"></span>
-            <p className="text-sm pt-6">GitHub</p>
-          </div>
-          <div className="flex-col justify-between items-center gap-4">
+        <section className="flex flex-wrap justify-center items-center gap-8 pt-4 pb-8">
+          {/* <div className="flex flex-col">
+            <span className="devicon-github-original text-6xl text-stone-700 dark:text-white"></span>
+            <p className="">GitHub</p>
+          </div> */}
+          <div className="flex flex-col gap-4">
             <img
-              className=""
+              className="w-20 mx-auto"
               src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
             />
-            <p className="text-sm">HTML5</p>
+            <p className="">HTML5</p>
           </div>
-          <div className="flex-col justify-between items-center gap-4">
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
-            <p className="text-sm">CSS</p>
+          <div className="flex flex-col gap-4">
+            <img
+              className="w-20 mx-auto" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
+            <p className="">CSS</p>
           </div>
-          <div className="flex-col justify-between items-center gap-4">
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-            <p className="text-sm">JavaScript</p>
+          <div className="flex flex-col gap-4">
+            <img
+              className="w-20 mx-auto" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+            <p className="">JavaScript</p>
           </div>
-          <div className="flex-col justify-between items-center gap-4">
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
-            <p className="text-sm">React</p>
+          <div className="flex flex-col gap-4">
+            <img
+              className="w-20 mx-auto" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+            <p className="">React</p>
           </div>
-          <div className="flex-col justify-between items-center gap-4">
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" />
-            <p className="text-sm">Tailwind</p>
+          <div className="flex flex-col gap-4">
+            <img
+              className="w-20 mx-auto" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" />
+            <p className="">Tailwind</p>
           </div>
-          <div className="flex-col justify-between items-center gap-4">
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
-            <p className="text-sm">NodeJS</p>
+          <div className="flex flex-col gap-4">
+            <img
+              className="w-20 mx-auto" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
+            <p className="">NodeJS</p>
           </div>
-          <div className="flex-col justify-between items-center">
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
-            <p className="text-sm">Git</p>
+          <div className="flex flex-col gap-4">
+            <img
+              className="w-20 mx-auto" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
+            <p className="">Git</p>
           </div>
         </section>
       </div>
