3950d9479678da56d355f4b6880e90d3c2cd2532
dpsiu <denversiu24@gmail.com>
2023-09-29 16:17:05 -0500

    Fix issue - project card content style alignment

--- Pages/Home.jsx
+++ Pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Skills } from "../Components/Skills"
+import { Skills } from "../Components/Skills";
 
 export function Home() {
   return (
@@ -15,13 +15,12 @@ export function Home() {
         <ul className="text-4xl flex justify-center gap-16 text-gray-600">
           <li>
             <a href="https://www.linkedin.com/in/denver-siu/">
-            <span className="devicon-linkedin-plain text-4xl text-indigo-500 dark:text-indigo-400 hover:text-orange-500 dark:hover:text-orange-400 transition duration-150"></span>
-              
+              <span className="devicon-linkedin-plain text-4xl text-indigo-500 dark:text-indigo-400 hover:text-orange-500 dark:hover:text-orange-400 transition duration-150"></span>
             </a>
           </li>
           <li>
             <a href="https://github.com/dpsiu">
-            <span className="devicon-github-original text-4xl text-indigo-500 dark:text-indigo-400 hover:text-orange-500 dark:hover:text-orange-400 transition duration-150"></span>
+              <span className="devicon-github-original text-4xl text-indigo-500 dark:text-indigo-400 hover:text-orange-500 dark:hover:text-orange-400 transition duration-150"></span>
             </a>
           </li>
         </ul>
@@ -49,6 +48,7 @@ export function Home() {
               width="1600"
               height="832"
               alt="shopcarthomepage"
+              className="h-70"
             />
           </a>
           <script
@@ -56,41 +56,42 @@ export function Home() {
             src="//embedr.flickr.com/assets/client-code.js"
             charSet="utf-8"
           ></script>
-          <p className="text-md text-left py-5 leading-8 text-stone-700 md:text-xl max-w-xl mx-auto dark:text-white">
-            A mock shopping cart application built using React with a focus in
-            demonstrating and applying various concepts such as component
-            structure, routing, state management, and manipulating API data.
-          </p>
-          <ul className="flex pt-2 pb-8 content-between gap-4 font-bold ">
-            <a
-              className="dark:hover:text-indigo-300 transition duration-200 hover:text-indigo-500"
-              href="https://denver-siu-shopping-cart.netlify.app/"
-            >
-              <li>Demo</li>
-            </a>
-            <li> - </li>
-
-            <a
-              className="dark:hover:text-indigo-300 transition duration-200 hover:text-indigo-500"
-              href="https://github.com/dpsiu/Shopping-Cart"
-            >
-              <li>GitHub</li>
-            </a>
-          </ul>
-          <ul className="flex flex-wrap text-sm text-gray-800 gap-2 max-w-fit">
-            <li className="bg-indigo-400 text-white py-2 px-4 rounded-3xl">
-              HTML
-            </li>
-            <li className="bg-indigo-400 text-white py-2 px-4 rounded-3xl">
-              CSS
-            </li>
-            <li className="bg-indigo-400 text-white py-2 px-4 rounded-3xl">
-              JavaScript
-            </li>
-            <li className="bg-indigo-400 text-white py-2 px-4 rounded-3xl">
-              React
-            </li>
-          </ul>
+          <div className="mx-auto md:p-6">
+            <p className="text-md text-left py-5 leading-8 text-stone-700 md:text-xl  dark:text-white">
+              A mock shopping cart application built using React with a focus in
+              demonstrating and applying various concepts such as component
+              structure, routing, state management, and manipulating API data.
+            </p>
+            <ul className="flex pt-2 pb-8 content-between gap-4 font-bold ">
+              <a
+                className="dark:hover:text-indigo-300 transition duration-200 hover:text-indigo-500"
+                href="https://denver-siu-shopping-cart.netlify.app/"
+              >
+                <li>Demo</li>
+              </a>
+              <li> - </li>
+              <a
+                className="dark:hover:text-indigo-300 transition duration-200 hover:text-indigo-500"
+                href="https://github.com/dpsiu/Shopping-Cart"
+              >
+                <li>GitHub</li>
+              </a>
+            </ul>
+            <ul className="flex flex-wrap text-sm text-gray-800 gap-2 max-w-fit">
+              <li className="bg-indigo-400 text-white py-2 px-4 rounded-3xl">
+                HTML
+              </li>
+              <li className="bg-indigo-400 text-white py-2 px-4 rounded-3xl">
+                CSS
+              </li>
+              <li className="bg-indigo-400 text-white py-2 px-4 rounded-3xl">
+                JavaScript
+              </li>
+              <li className="bg-indigo-400 text-white py-2 px-4 rounded-3xl">
+                React
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="text-center shadow-lg shadow-slate-400 p-10 rounded-xl my-10 mx-auto max-w-3xl dark:text-white dark:shadow-slate-800 dark:bg-zinc-600">
           <h3 className="text-xl font-semibold pt-8 pb-6">
@@ -106,6 +107,7 @@ export function Home() {
               width="1600"
               height="832"
               alt="cvapphomepage"
+              className="h-70"
             />
           </a>
           <script
@@ -113,40 +115,42 @@ export function Home() {
             src="//embedr.flickr.com/assets/client-code.js"
             charSet="utf-8"
           ></script>
-          <p className="text-md text-left py-5 leading-8 text-stone-700 md:text-xl max-w-xl mx-auto dark:text-white">
-            A CV Application Creator that takes user inputs and returns an
-            example resume. The projects serves as an application of React best
-            principles, components, and state management.
-          </p>
-          <ul className="flex pt-2 pb-8 content-between gap-4 font-bold ">
-            <a
-              className="dark:hover:text-indigo-300 transition duration-200 hover:text-indigo-500"
-              href="https://denver-siu-cv-application.netlify.app/"
-            >
-              <li>Demo</li>
-            </a>
-            <li> - </li>
-            <a
-              className="dark:hover:text-indigo-300 transition duration-200 hover:text-indigo-500"
-              href="https://github.com/dpsiu/CV-Application"
-            >
-              <li>GitHub</li>
-            </a>
-          </ul>
-          <ul className="flex flex-wrap text-sm text-gray-800 gap-2 max-w-fit">
-            <li className="bg-indigo-400 text-white py-2 px-4 rounded-3xl">
-              HTML
-            </li>
-            <li className="bg-indigo-400 text-white py-2 px-4 rounded-3xl">
-              CSS
-            </li>
-            <li className="bg-indigo-400 text-white py-2 px-4 rounded-3xl">
-              JavaScript
-            </li>
-            <li className="bg-indigo-400 text-white py-2 px-4 rounded-3xl">
-              React
-            </li>
-          </ul>
+          <div className="mx-auto md:p-6">
+            <p className="text-md text-left py-5 leading-8 text-stone-700 md:text-xl dark:text-white">
+              A CV Application Creator that takes user inputs and returns an
+              example resume. The projects serves as an application of React
+              best principles, components, and state management.
+            </p>
+            <ul className="flex pt-2 pb-8 content-between gap-4 font-bold ">
+              <a
+                className="dark:hover:text-indigo-300 transition duration-200 hover:text-indigo-500"
+                href="https://denver-siu-cv-application.netlify.app/"
+              >
+                <li>Demo</li>
+              </a>
+              <li> - </li>
+              <a
+                className="dark:hover:text-indigo-300 transition duration-200 hover:text-indigo-500"
+                href="https://github.com/dpsiu/CV-Application"
+              >
+                <li>GitHub</li>
+              </a>
+            </ul>
+            <ul className="flex flex-wrap text-sm text-gray-800 gap-2 max-w-fit">
+              <li className="bg-indigo-400 text-white py-2 px-4 rounded-3xl">
+                HTML
+              </li>
+              <li className="bg-indigo-400 text-white py-2 px-4 rounded-3xl">
+                CSS
+              </li>
+              <li className="bg-indigo-400 text-white py-2 px-4 rounded-3xl">
+                JavaScript
+              </li>
+              <li className="bg-indigo-400 text-white py-2 px-4 rounded-3xl">
+                React
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <Skills />
